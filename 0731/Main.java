7be31de2706c5ee3ee6f09aa7f71db763cab235c
Cr-Learning <2421233529@qq.com>
2018-08-23 18:23:26 +0800

    java程序

---
+++ 0731/Main.java
@@ -0,0 +1,18 @@
+
+
+/**
+: javac Main.java 理解这个过程、生成什么文件
+： java Main 理解
+*/
+
+/**
+JAVA_HOME
+JAVA_CLASSPATH 配置
+会用命令行比编译
+*/
+public class Main{
+
+	public static void main(String args[]){
+		System.out.println("Hello world");
+	}
+}
